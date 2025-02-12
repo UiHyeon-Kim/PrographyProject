6ab83f5ab9991b1e3a4337ca2b5bfc13c98b5518
김의현 <hyunkim6790@gmail.com>
2025-02-12 21:22:13 +0900

    build: navigation 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.hanpro.prographyproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hanpro.prographyproject"
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
