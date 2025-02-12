a45cca33772810f71c78b0633fb192f64da612eb
김의현 <hyunkim6790@gmail.com>
2025-02-12 22:04:30 +0900

    feat(Screen): 앱 구성 화면 임시 구현

---
+++ app/src/main/java/com/hanpro/prographyproject/ui/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.hanpro.prographyproject.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+}
---
+++ app/src/main/java/com/hanpro/prographyproject/ui/PhotoDetailScreen.kt
@@ -0,0 +1,7 @@
+package com.hanpro.prographyproject.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PhotoDetailScreen() {
+}
