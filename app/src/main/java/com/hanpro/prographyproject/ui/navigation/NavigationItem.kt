0498ad1411ee9195447415926216a10407e91bc5
김의현 <hyunkim6790@gmail.com>
2025-02-12 22:54:01 +0900

    feat(NavigationItem): 타이틀, 아이콘 정보 추가

--- app/src/main/java/com/hanpro/prographyproject/ui/navigation/NavigationItem.kt
+++ app/src/main/java/com/hanpro/prographyproject/ui/navigation/NavigationItem.kt
@@ -1,6 +1,13 @@
 package com.hanpro.prographyproject.ui.navigation
 
-sealed class NavigationItem(val route: String) {
-    data object Home : NavigationItem("home")
-    data object RandomPhoto : NavigationItem("randomPhoto")
+import androidx.annotation.DrawableRes
+import com.hanpro.prographyproject.R
+
+sealed class NavigationItem(
+    val route: String,
+    val title: String,
+    @DrawableRes val icon: Int,
+) {
+    data object Home : NavigationItem("home", "Home", R.drawable.house)
+    data object RandomPhoto : NavigationItem("randomPhoto", "Random Photo", R.drawable.cards)
 }
