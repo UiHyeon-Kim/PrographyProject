efb075bd2f3c28fa34946ae4e6d9716f9b20ee42
김의현 <hyunkim6790@gmail.com>
2025-02-12 22:55:39 +0900

    feat(BottomNavigation): 네비게이션바 구현

---
+++ app/src/main/java/com/hanpro/prographyproject/ui/navigation/BottomNavigation.kt
@@ -0,0 +1,44 @@
+package com.hanpro.prographyproject.ui.navigation
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigation(navController: NavHostController) {
+    val navItems = listOf(
+        NavigationItem.Home,
+        NavigationItem.RandomPhoto
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationBar {
+        navItems.forEach { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = item.icon),
+                        contentDescription = item.title
+                    )
+                },
+                selected = currentRoute == item.route,
+                onClick = {
+                    if (currentRoute != item.route) {
+                        navController.navigate(item.route) {
+                            popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
