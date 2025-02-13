aa001c921bd2ab9a3a597268449397c5648853d6
김의현 <hyunkim6790@gmail.com>
2025-02-13 21:25:31 +0900

    feat(RetrofitClient): Retrofit 객체 생성

---
+++ app/src/main/java/com/hanpro/prographyproject/data/source/remote/RetrofitClient.kt
@@ -0,0 +1,36 @@
+package com.hanpro.prographyproject.data.source.remote
+
+import com.hanpro.prographyproject.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.unsplash.com/"
+    private const val UNSPLASH_KEY = BuildConfig.UNSPLASH_KEY
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .addInterceptor { chain ->
+            val original = chain.request()
+            val request = original.newBuilder()
+                .header("Authorization", "Client-ID $UNSPLASH_KEY")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
+
+    val unsplashApi: UnsplashApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UnsplashApi::class.java)
+    }
+}
