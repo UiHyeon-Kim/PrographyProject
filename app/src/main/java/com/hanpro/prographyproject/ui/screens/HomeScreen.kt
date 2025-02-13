6f06e4390284c62766408917a82c88a27292b7ed
김의현 <hyunkim6790@gmail.com>
2025-02-13 16:03:09 +0900

    feat(HomeScreen): 북마크, 최신 이미지 텍스트 구현

--- app/src/main/java/com/hanpro/prographyproject/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/hanpro/prographyproject/ui/screens/HomeScreen.kt
@@ -1,21 +1,57 @@
 package com.hanpro.prographyproject.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier.padding(top = 20.dp, bottom = 20.dp)
     ) {
-        Text("test")
+        Box(
+            modifier = Modifier
+                .padding(start = 20.dp, top = 10.dp, end = 20.dp, bottom = 9.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "북마크",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF070707),
+                    // 볼드체?
+                )
+            )
+        }
+
+        // TODO: 북마크 리스트
+
+        Box(
+            modifier = Modifier
+                .padding(start = 20.dp, top = 10.dp, end = 20.dp, bottom = 9.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "최신 이미지",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF070707)
+                )
+            )
+        }
+
+        // TODO: 최신 이미지 리스트
     }
 }
