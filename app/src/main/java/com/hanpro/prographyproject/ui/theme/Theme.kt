8c6d5042c7a6f1e4ffd13c3c00a9f0ee9b909620
김의현 <hyunkim6790@gmail.com>
2025-02-15 01:06:52 +0900

    design(Theme): LightTheme만 사용하도록 적용

--- app/src/main/java/com/hanpro/prographyproject/ui/theme/Theme.kt
+++ app/src/main/java/com/hanpro/prographyproject/ui/theme/Theme.kt
@@ -1,13 +1,7 @@
 package com.hanpro.prographyproject.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -35,7 +29,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun PrographyProjectTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
