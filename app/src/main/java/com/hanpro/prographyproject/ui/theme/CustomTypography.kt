af3402e68bc91b6cf11ca93bfc911d2547da1823
김의현 <hyunkim6790@gmail.com>
2025-02-15 20:08:26 +0900

    feat(CustomTypography): Pretendard 폰트 주입

---
+++ app/src/main/java/com/hanpro/prographyproject/ui/theme/CustomTypography.kt
@@ -0,0 +1,11 @@
+package com.hanpro.prographyproject.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.hanpro.prographyproject.R
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+)
