0a18acc0d1fa0ac777d0d50b16870248839faadb
김의현 <hyunkim6790@gmail.com>
2025-02-18 01:04:07 +0900

    feat(AppNavHost): 내비게이션 매개변수 주입

--- app/src/main/java/com/hanpro/prographyproject/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/hanpro/prographyproject/ui/navigation/AppNavHost.kt
@@ -2,8 +2,10 @@ package com.hanpro.prographyproject.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.hanpro.prographyproject.ui.screens.HomeScreen
 import com.hanpro.prographyproject.ui.screens.PhotoDetailScreen
 import com.hanpro.prographyproject.ui.screens.RandomPhotoScreen
@@ -14,10 +16,21 @@ fun AppNavHost(navController: NavHostController) {
         navController = navController,
         startDestination = NavigationItem.Home.route
     ) {
-        composable(NavigationItem.Home.route) { HomeScreen() }
+        composable(NavigationItem.Home.route) { HomeScreen(navController = navController) }
+
         composable(NavigationItem.RandomPhoto.route) {
             RandomPhotoScreen(onBookmarkAdd = {}, onLoadMore = {})
         }
-        composable("photoDetail") { PhotoDetailScreen() }
+
+        composable(
+            "photoDetail/{photoId}",
+            arguments = listOf(navArgument("photoId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val photoId = backStackEntry.arguments?.getString("photoId") ?: ""
+            PhotoDetailScreen(
+                photoId = photoId,
+                onClose = { navController.popBackStack() }
+            )
+        }
     }
 }
