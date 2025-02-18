194f70e65d63a54035fc38ac3af4f8a6715c1ad6
김의현 <hyunkim6790@gmail.com>
2025-02-19 02:51:29 +0900

    refactor(BookmarkDao, BookmarkRepository): 북마크 실시간 업데이트를 확인하기 위해 Flow값 반환하도록 변경

--- app/src/main/java/com/hanpro/prographyproject/data/source/local/BookmarkDao.kt
+++ app/src/main/java/com/hanpro/prographyproject/data/source/local/BookmarkDao.kt
@@ -1,12 +1,13 @@
 package com.hanpro.prographyproject.data.source.local
 
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BookmarkDao {
 
     @Query("SELECT * FROM bookmarks")
-    suspend fun getAllBookmarks(): List<Bookmark>
+    fun getAllBookmarks(): Flow<List<Bookmark>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBookmark(bookmark: Bookmark)
--- app/src/main/java/com/hanpro/prographyproject/domain/repository/BookmarkRepository.kt
+++ app/src/main/java/com/hanpro/prographyproject/domain/repository/BookmarkRepository.kt
@@ -2,12 +2,13 @@ package com.hanpro.prographyproject.domain.repository
 
 import com.hanpro.prographyproject.data.source.local.Bookmark
 import com.hanpro.prographyproject.data.source.local.BookmarkDao
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class BookmarkRepository @Inject constructor(
     private val bookmarkDao: BookmarkDao
 ) {
-    suspend fun getBookmarks(): List<Bookmark> = bookmarkDao.getAllBookmarks()
+    fun getBookmarks(): Flow<List<Bookmark>> = bookmarkDao.getAllBookmarks()
 
     suspend fun addBookmark(bookmark: Bookmark) = bookmarkDao.insertBookmark(bookmark)
 
