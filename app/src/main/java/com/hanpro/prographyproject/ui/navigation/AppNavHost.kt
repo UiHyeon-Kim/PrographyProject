e904dd3234b881d6c500b017042b845e19c79983
김의현 <hyunkim6790@gmail.com>
2025-02-19 03:02:58 +0900

    refactor(AppNavHost): PhotoDetailDialog로 바뀜에 따라 내비 호스트에서 제거

--- app/src/main/java/com/hanpro/prographyproject/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/hanpro/prographyproject/ui/navigation/AppNavHost.kt
@@ -2,12 +2,9 @@ package com.hanpro.prographyproject.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import com.hanpro.prographyproject.ui.screens.HomeScreen
-import com.hanpro.prographyproject.ui.screens.PhotoDetailScreen
 import com.hanpro.prographyproject.ui.screens.RandomPhotoScreen
 
 @Composable
@@ -20,17 +17,6 @@ fun AppNavHost(navController: NavHostController) {
 
         composable(NavigationItem.RandomPhoto.route) {
             RandomPhotoScreen(onBookmarkAdd = {}, onLoadMore = {})
-        }
-
-        composable(
-            "photoDetail/{photoId}",
-            arguments = listOf(navArgument("photoId") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val photoId = backStackEntry.arguments?.getString("photoId") ?: ""
-            PhotoDetailScreen(
-                photoId = photoId,
-                onClose = { navController.popBackStack() }
-            )
         }
     }
 }
