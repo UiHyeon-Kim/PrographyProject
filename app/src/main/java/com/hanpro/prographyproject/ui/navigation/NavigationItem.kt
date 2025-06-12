853aa7910aa98bfc621f6f45c9c18b6f6c92c82d
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-06-12 22:53:55 +0900

    refactor: Dialog 를 위한 DetailPhoto 아이템 객체 생성

--- app/src/main/java/com/hanpro/prographyproject/ui/navigation/NavigationItem.kt
+++ app/src/main/java/com/hanpro/prographyproject/ui/navigation/NavigationItem.kt
@@ -6,8 +6,9 @@ import com.hanpro.prographyproject.R
 sealed class NavigationItem(
     val route: String,
     val title: String,
-    @DrawableRes val icon: Int,
+    @DrawableRes val icon: Int?,
 ) {
     data object Home : NavigationItem("home", "Home", R.drawable.house)
     data object RandomPhoto : NavigationItem("randomPhoto", "Random Photo", R.drawable.cards)
+    data object DetailPhoto : NavigationItem("detailPhoto", "Detail Photo", null)
 }
