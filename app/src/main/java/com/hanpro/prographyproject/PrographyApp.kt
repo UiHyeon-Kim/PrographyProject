9676a969015085f6448c63aa218369bcfaa6c74b
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-06-12 22:42:19 +0900

    refactor: NavHost만 따로 구분한 코드를 앱 전체 구조에 포함해 작성
    - HomeScreen, RandomPhotoScreen 을 통해 BottomNavigation 에 navController 를 넘기던 불편함을 없애고, 바로 전달하도록 수정

---
+++ app/src/main/java/com/hanpro/prographyproject/PrographyApp.kt
@@ -0,0 +1,49 @@
+package com.hanpro.prographyproject
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
+import com.hanpro.prographyproject.ui.components.BottomNavigation
+import com.hanpro.prographyproject.ui.components.TopBar
+import com.hanpro.prographyproject.ui.dialog.PhotoDetailDialog
+import com.hanpro.prographyproject.ui.navigation.AppNavigation
+import com.hanpro.prographyproject.ui.navigation.NavigationItem.DetailPhoto
+import com.hanpro.prographyproject.ui.navigation.NavigationItem.Home
+import com.hanpro.prographyproject.ui.navigation.NavigationItem.RandomPhoto
+import com.hanpro.prographyproject.ui.screens.HomeScreen
+import com.hanpro.prographyproject.ui.screens.RandomPhotoScreen
+
+@Composable
+fun PrographyApp() {
+    val navController = rememberNavController()
+    val appNavigation = remember(navController) { AppNavigation(navController) }
+
+    CompositionLocalProvider(LocalNavigation provides appNavigation) {
+        Scaffold(
+            topBar = { TopBar() },
+            bottomBar = { BottomNavigation(navController) }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = Home.route,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                // TODO: 컨트롤러 대신 이동 함수 넣기
+                composable(Home.route) { HomeScreen() }
+                composable(RandomPhoto.route) { RandomPhotoScreen() }
+                dialog(DetailPhoto.route) { PhotoDetailDialog(onClose = {}) }
+            }
+        }
+    }
+}
+
+val LocalNavigation = staticCompositionLocalOf<AppNavigation?> { null }
+
