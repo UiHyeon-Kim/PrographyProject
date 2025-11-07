739b67207e269d1828f13ab210e3d08b7c560b0a
김의현 <hyunkim6790@gmail.com>
2025-11-07 23:44:43 +0900

    feat: 북마크 유스케이스 구현

---
+++ app/src/main/java/com/hanpro/prographyproject/domain/usecase/DeleteBookmarkUseCase.kt
@@ -0,0 +1,11 @@
+package com.hanpro.prographyproject.domain.usecase
+
+import com.hanpro.prographyproject.data.source.local.Bookmark
+import com.hanpro.prographyproject.domain.repository.BookmarkRepository
+import javax.inject.Inject
+
+class DeleteBookmarkUseCase @Inject constructor(
+    private val bookmarkRepository: BookmarkRepository
+) {
+    suspend operator fun invoke(bookmark: Bookmark) = bookmarkRepository.deleteBookmark(bookmark)
+}
