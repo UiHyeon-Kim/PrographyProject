7a0501570e68356bc3730b2fdf55308c77a66763
김의현 <hyunkim6790@gmail.com>
2025-11-28 01:49:14 +0900

    style: 데이터 타입 명시적 선언 및 코드 일관성 유지

--- app/src/main/java/com/hanpro/prographyproject/ui/screens/NoNetworkScreen.kt
+++ app/src/main/java/com/hanpro/prographyproject/ui/screens/NoNetworkScreen.kt
@@ -5,17 +5,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CloudOff
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hanpro.prographyproject.ui.theme.PrographyProjectTheme
 
 @Composable
 fun NoNetworkScreen(
@@ -28,7 +23,7 @@ fun NoNetworkScreen(
     ) {
         Icon(
             imageVector = Icons.Default.CloudOff,
-            contentDescription = "network disconnect",
+            contentDescription = "네트워크 연결 끊김",
             modifier = Modifier.size(80.dp),
             tint = Color.Gray
         )
@@ -44,22 +39,14 @@ fun NoNetworkScreen(
             onClick = onRefresh,
             modifier = Modifier.padding(4.dp),
             shape = RoundedCornerShape(8.dp),
-            border = BorderStroke(1.dp, Color.Black),
+            border = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface),
             contentPadding = PaddingValues(horizontal = 14.dp, vertical = 4.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color.Transparent,
-                contentColor = Color.Black,
+                contentColor = MaterialTheme.colorScheme.onSurface,
             ),
         ) {
             Text("새로고침")
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun NoNetwork() {
-    PrographyProjectTheme {
-        NoNetworkScreen {  }
     }
 }
